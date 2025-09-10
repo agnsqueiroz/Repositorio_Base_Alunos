dc8e670b4041fbe0b8e2d9b1d6b573e3f8053c9a
agnsqueiroz <agnsqueiroz@gmail.com>
2025-09-10 14:12:47 -0300

    Add files via upload

---
+++ Modulo 1/Listas/listas.py
@@ -0,0 +1,29 @@
+# Lista inicial 
+nomes = ["Joaquim", "Maria", "Ana"]
+print("Lista inicial:", nomes)
+
+#Adicionando elementos 
+nomes.append("Carlos") #Adiciona ao final
+print("Após append:", nomes)
+
+nomes.insert(1, "Fernanda") #Insere "Fernanda" na posição 1
+print("Após insert:", nomes)
+
+#Modificando Elementos
+nomes[2] = "Paulo" #Modifica o elemento no índice 2
+print("Após modificação:", nomes)
+
+
+
+#Removendo elementos 
+del nomes [3] #Remove o elemento do índice 3
+print("Após del:", nomes)
+
+nomes.remove("Maria") # Remove a primeira ocorreência de "Maria"
+print("Após remove", nomes)
+
+removido = nomes.pop(2) #Remove e retorna o elemnto da índice 2
+print(f"Após pop(removido'{removido}'):", nomes)
+
+nomes.clear() #Esvazia a lista
+print("Após clear:", nomes)
