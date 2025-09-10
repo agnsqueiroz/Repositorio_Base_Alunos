0284a106c073ebceb51c17545de9bc6e07c9f0b2
agnsqueiroz <agnsqueiroz@gmail.com>
2025-09-10 14:01:06 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/atividade5 (1).py
@@ -0,0 +1,7 @@
+velocidade_carro = 85
+velocidade_maxima = 80
+
+if velocidade_carro > velocidade_maxima:
+    print("O carro está acima do limite de velocidade. ")
+else:
+    print("O carro está dentro do limite de velocidade. ")
